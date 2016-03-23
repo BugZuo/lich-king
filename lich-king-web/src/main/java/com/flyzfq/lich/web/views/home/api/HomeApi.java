303226fed4f759846dec3dbcae662a2f075432db
zuozuo <fenqiang.zuo@duitang.com>
2016-03-23 15:12:25 +0800

    refine the logback

--- lich-king-web/src/main/java/com/flyzfq/lich/web/views/home/api/HomeApi.java
+++ lich-king-web/src/main/java/com/flyzfq/lich/web/views/home/api/HomeApi.java
@@ -3,6 +3,8 @@ package com.flyzfq.lich.web.views.home.api;
 import com.flyzfq.lich.biz.security.LoginUtil;
 import com.flyzfq.lich.model.user.LoginUser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class HomeApi {
 
+  private static final Logger logger = LoggerFactory.getLogger(HomeApi.class);
+
   @RequestMapping(value = "hi/", method = RequestMethod.GET)
   public String hi() {
     LoginUser loginUser = LoginUtil.getLoginUser();
-    return "hi" + ": " + loginUser != null ? loginUser.getUsername() : "shit";
+    logger.info("hi" + ": " + (loginUser != null ? loginUser.getUsername() : "shit"));
+    return "hi" + ": " + (loginUser != null ? loginUser.getUsername() : "shit");
   }
 }
